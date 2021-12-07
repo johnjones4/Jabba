072faa19b99ca4ccb2204a402463fbc484471c9d
John Jones <john@johnjonesfour.com>
2021-12-06 22:20:01 -0500

    hash

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes } from 'react-router'
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import './App.css'
 import EventsWrapper from './pages/Events/EventsWrapper';
@@ -8,13 +8,13 @@ import EventWrapper from './pages/Event/EventWrapper';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/events" element={<EventsWrapper />} />
         <Route path="/event/:id" element={<EventWrapper />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
